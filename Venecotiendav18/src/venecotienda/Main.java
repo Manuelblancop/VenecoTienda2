ff4286ed8e22a37546eccb7e969e93076fbfc048
udam1305 <adamarmoza@gmail.com>
2025-06-19 12:15:33 -0300

    Solucion xd
    skere

--- Venecotiendav18/src/venecotienda/Main.java
+++ Venecotiendav18/src/venecotienda/Main.java
@@ -2,14 +2,21 @@ package venecotienda;
 
 import javax.swing.SwingUtilities;
 import GUI.Login;
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
         	Login loginFrame = new Login();
             loginFrame.setVisible(true);
+<<<<<<< Updated upstream
+=======
+            
+>>>>>>> Stashed changes
         });
     }
 }
